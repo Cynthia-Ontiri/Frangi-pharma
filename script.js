da8cba32c7c88af202f7746b00f757e701a6f6b4
Cynthia-Ontiri <cynthia.otiri@students.moringaschool.com>
2022-09-09 10:15:16 +0300

    done with the project

--- script.js
+++ script.js
@@ -1,29 +1,24 @@
-const drugCardTemplate = document.querySelector("[data-drug-template]")
-const drugCardContainer = document.querySelector(["data-drug-cards-container"])
-const searchInput = document.querySelector("[data-search]")
+const apiUrl = ("http: //localhost:3000");
 
 let drugs = []
-console.log(searchInput)
-searchInput.addEventListener("input", e => {
-  const value = e.target.value.toLowerCase()
-  drugs.forEach(drug => {
-    const isVisible = drug.name.toLowerCase().includes(value) || 
-    drug.count.toLowerCase().includes(value)
-    drug.element.classList.toggle("hide", !isVisible)
-  })
-})
+
+function getDrugDetails(){
 fetch("https://api.fda.gov/drug/label.json?count=openfda.brand_name.exact&limit=1000")
   .then(res => res.json())
-  .then(drug => {
-    drugs = drug.map(drug => {
-      const card = drugCardTemplate.cloneNode(true).children[0]
-      const header = card.querySelector("[data-header]")
-      const body = card.querySelector("[data-body]")
-      header.textContent = drug.term
-      body.textContent = drug.count
-      drugCardContainer.append(card)
-      return { term: drug.term, count: drug.count}
-    })
+  .then(data => {
+    document.getElementById("drug-term").innerText = data.term
+    document.getElementById("drug-count").innerText = data.term
   })
-    
- 
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  getDrugs();
+  getDrugDetails(1);
+//submit inquiry
+  document.getElementById("submit-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    document.getElementById("inquiry-list").innerHTML += `<li>
+    ${document.getElementById("inquiry").value}
+    </li>`;
+  });
+})
